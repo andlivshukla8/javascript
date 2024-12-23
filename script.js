516079a2ba3decdcb489f3e5d51585fb0a299799
andlivshukla8 <andlivshukla72@gamil.com>
2024-12-23 15:36:24 +0530

    doneeee

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
-function sayHello(){
-  alert("Hello World")
-}
+// function sayHello(){
+//   alert("Hello World")
+// }
+// function show()
+// {
+//   var a="A is local outer variable";
+//  document.write(a+"<br>")
+// }
+// function disk()
+// {
+//   var a="B is local outer variable";
+//   document.write(b+"<br>")
+// }
+// var name='John';
+// function first()
+// {
+//   var a='hello';
+//   second();
+//   var x=a+name;
+//   console.log(x);
+// }
+// function second()
+// {
+//   var b='hi';
+//   third();
+//   var z=b+name;
+//   console.log(z);
+// }
+// function third()
+// {
+//   var c='how are you';
+//   var y=c+name;
+//   console.log(y);
+// }
+// first();
+// disk();
+// }show();
+
+// function add(a,b)
+// {
+// console.log(a)
+// }
